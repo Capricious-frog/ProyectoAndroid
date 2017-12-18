de3942b21f8c42abb7aaadcbc8a82cbbbe8444ae
Julen Ortiz <Julortizw@gmail.com>
2017-12-18 22:54:35 +0100

    Eliminar un import inecesario

--- app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
@@ -1,6 +1,5 @@
 package com.example.adminportatil.proyectoandroid;
 
-import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
