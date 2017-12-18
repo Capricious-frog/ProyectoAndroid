0385076a1147e9036fc24fa2f5bbd9bd9602849c
Julen Ortiz <Julortizw@gmail.com>
2017-12-18 22:11:48 +0100

    Actualizar codigo antiguo Arreglar errores Añadir la funcion anadir_fila2 Añadir el boton que ejecuta anadir_fila2 Eliminar codigo redundante Cambiar los nombre de los botones a unos mas consistentes

--- app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
@@ -2,6 +2,7 @@ package com.example.adminportatil.proyectoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
@@ -16,19 +17,28 @@ public class PedidoKebab extends AppCompatActivity {
 
     public void anadir_fila(View v) {
         /* Find Tablelayout defined in main.xml */
-        TableLayout tl = (TableLayout) findViewById(R.id.tablaKebabs);
+        TableLayout tl = findViewById(R.id.tablaKebabs);
         /* Create a new row to be added. */
         TableRow tr = new TableRow(this);
-        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
         /* Create a Button to be the row-content. */
         Button b = new Button(this);
         b.setText("Dynamic Button");
-        b.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        b.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
         /* Add Button to row. */
         tr.addView(b);
         /* Add row to TableLayout. */
         //tr.setBackgroundResource(R.drawable.sf_gradient_03);
-        tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+        tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+    }
+
+    public void anadir_fila2(View v) {
+        TableLayout tl = findViewById(R.id.tablaKebabs);
+
+        LayoutInflater inflator = PedidoKebab.this.getLayoutInflater();
+        TableRow rowView = new TableRow(v.getContext());
+        inflator.inflate(R.layout.activity_pedido_kebab, rowView);
+        tl.addView(rowView);
     }
 
     @Override
