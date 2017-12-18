85b3e446f3b4506faa2f06ded29155a1527c754b
Julen Ortiz <Julortizw@gmail.com>
2017-12-18 10:33:20 +0100

    Añadido table layout y boton para crear un table row

--- app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
@@ -3,12 +3,32 @@ package com.example.adminportatil.proyectoandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class PedidoKebab extends AppCompatActivity {
 
     public void cerrar(View v){
         finish();
         System.exit(0);
+    }
+
+    public void anadir_fila(View v) {
+        /* Find Tablelayout defined in main.xml */
+        TableLayout tl = (TableLayout) findViewById(R.id.tablaKebabs);
+        /* Create a new row to be added. */
+        TableRow tr = new TableRow(this);
+        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        /* Create a Button to be the row-content. */
+        Button b = new Button(this);
+        b.setText("Dynamic Button");
+        b.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        /* Add Button to row. */
+        tr.addView(b);
+        /* Add row to TableLayout. */
+        //tr.setBackgroundResource(R.drawable.sf_gradient_03);
+        tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
     }
 
     @Override
