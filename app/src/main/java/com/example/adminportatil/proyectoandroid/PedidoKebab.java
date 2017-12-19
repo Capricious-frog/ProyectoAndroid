0619738eef1b97e704a70ead775335556e77afe7
Julen Ortiz <Julortizw@gmail.com>
2017-12-19 09:40:41 +0100

    Poco para el tiempo que me ha llevado

--- app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
@@ -7,8 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
 
 public class PedidoKebab extends AppCompatActivity {
+
+    int contador = 1;
 
     public void cerrar(View v){
         finish();
@@ -16,23 +19,34 @@ public class PedidoKebab extends AppCompatActivity {
     }
 
     public void anadir_fila(View v) {
+        if (contador >= 30) {
+            contador++;
+
         /* Find Tablelayout defined in main.xml */
-        TableLayout tl = findViewById(R.id.tablaKebabs);
+            TableLayout tl = findViewById(R.id.tablaKebabs);
         /* Create a new row to be added. */
-        TableRow tr = new TableRow(this);
-        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+            TableRow tr = new TableRow(this);
+            tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
         /* Create a Button to be the row-content. */
-        Button b = new Button(this);
-        b.setText("Dynamic Button");
-        b.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+            Button b = new Button(this);
+            b.setText("Dynamic Button");
+            b.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
         /* Add Button to row. */
-        tr.addView(b);
+            tr.addView(b);
         /* Add row to TableLayout. */
-        //tr.setBackgroundResource(R.drawable.sf_gradient_03);
-        tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+            //tr.setBackgroundResource(R.drawable.sf_gradient_03);
+            tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+
+        } else {
+            Toast toast = Toast.makeText(this, "No puedes añadir mas. Ya tienes todas las combinaciones posibles! :)", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
     }
 
     public void anadir_fila2(View v) {
+        contador++;
+
         TableLayout tl = findViewById(R.id.tablaKebabs);
 
         LayoutInflater inflator = PedidoKebab.this.getLayoutInflater();
