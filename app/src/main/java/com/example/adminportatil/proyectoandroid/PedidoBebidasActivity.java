d9b9865d9e03bb2caa19bf0fc1ee8fe835239df7
Julen Ortiz <Julortizw@gmail.com>
2018-01-03 12:31:46 +0100

    Creacion de pedido bebidas Crear los elementos de la actividad de pedido bebidas y el codigo necesario. Hacer que el boton de siguente funcione en pedido kebab. Creacion de la actividad resumen pedido. Añadir los botones cancelar y aceptar en resumen pedido.

--- app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoBebidasActivity.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoBebidasActivity.java
@@ -1,9 +1,22 @@
 package com.example.adminportatil.proyectoandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class PedidoBebidasActivity extends AppCompatActivity {
+
+    public void cerrar(View v){
+        finish();
+        System.exit(0);
+    }
+
+    public void lanzarResumentPedido(View view){
+            Intent i = new Intent(this, ResumenPedido.class);
+            startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
@@ -1,10 +1,10 @@
 package com.example.adminportatil.proyectoandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
@@ -16,6 +16,11 @@ public class PedidoKebab extends AppCompatActivity {
     public void cerrar(View v){
         finish();
         System.exit(0);
+    }
+
+    public void lanzarPedidoKebab(View view){
+            Intent i = new Intent(this, PedidoBebidasActivity.class);
+            startActivity(i);
     }
 
     public void anadir_fila(View v) {
