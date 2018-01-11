ab455439a5d4f1e80f8fea29eacd47ae1675ce66
Axirpo <a.uranga.esnal@hotmail.com>
2018-01-11 12:03:47 +0100

    borrados botones de cerrar y siguiente

--- app/src/main/java/com/example/adminportatil/proyectoandroid/Portada.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/Portada.java
@@ -1,26 +1,12 @@
 package com.example.adminportatil.proyectoandroid;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class Portada extends AppCompatActivity {
 
-
-    public void cerrar(View v){
-        finish();
-        System.exit(0);
-    }
-
-    public void lanzarDatosCliente(View view){
-        Intent i = new Intent(this, DatosCliente.class);
-        startActivity(i);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
