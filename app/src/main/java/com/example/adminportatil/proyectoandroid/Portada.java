9230427d1b5f2bbfed4692899e2283e6280fc3e1
Julen Ortiz <Julortizw@gmail.com>
2018-01-11 12:03:02 +0100

    La portada cambia despues de 5s

--- app/src/main/java/com/example/adminportatil/proyectoandroid/Portada.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/Portada.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Portada extends AppCompatActivity {
 
 
@@ -21,6 +24,12 @@ public class Portada extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), DatosCliente.class));
+            }
+        }, 5000);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
