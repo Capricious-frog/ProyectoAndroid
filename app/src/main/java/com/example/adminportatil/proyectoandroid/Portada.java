11a187ad449e6495ac4d66ac4618a9b9a3ec08f6
Axirpo <a.uranga.esnal@hotmail.com>
2018-01-12 00:22:17 +0100

    fondo de pantalla

--- app/src/main/java/com/example/adminportatil/proyectoandroid/Portada.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/Portada.java
@@ -4,12 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Portada extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), DatosCliente.class));
+            }
+        }, 5000);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
