2934d5e68b6e239249df814843dabd1489cc8165
Julen Ortiz <Julortizw@gmail.com>
2018-01-11 12:10:20 +0100

    Borrado los botones

--- app/src/main/java/com/example/adminportatil/proyectoandroid/Portada.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/Portada.java
@@ -3,23 +3,12 @@ package com.example.adminportatil.proyectoandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Portada extends AppCompatActivity {
 
-
-    public void cerrar(View v){
-        finish();
-        System.exit(0);
-    }
-
-    public void lanzarDatosCliente(View view){
-        Intent i = new Intent(this, DatosCliente.class);
-        startActivity(i);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
