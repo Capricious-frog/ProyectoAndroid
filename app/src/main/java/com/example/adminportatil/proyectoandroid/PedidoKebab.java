3b43e1a7a61a82185c7721b89b23f7dd3453ea0a
Julen Ortiz <Julortizw@gmail.com>
2018-01-19 13:18:04 +0100

    Quitado importaciones innecesarias

--- app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoKebab.java
@@ -1,12 +1,10 @@
 package com.example.adminportatil.proyectoandroid;
 
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
