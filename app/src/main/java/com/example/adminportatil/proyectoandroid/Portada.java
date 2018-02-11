7de0aed750649c059684f9c8bd037d414b2f2550
Julen Ortiz <Julortizw@gmail.com>
2018-02-11 20:14:28 +0100

    ...

--- app/src/main/java/com/example/adminportatil/proyectoandroid/Portada.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/Portada.java
@@ -1,6 +1,8 @@
 package com.example.adminportatil.proyectoandroid;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,11 +11,12 @@ import java.util.TimerTask;
 
 public class Portada extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
+
+        añadirDatos();
 
         new Timer().schedule(new TimerTask() {
             @Override
@@ -22,6 +25,27 @@ public class Portada extends AppCompatActivity {
             }
         }, 3000);
 
+    }
+
+    public void añadirDatos(){
+        KebabsSQLiteHelper kqlh = new KebabsSQLiteHelper(this);
+        SQLiteDatabase db = kqlh.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        String[] bebidas, precio_bebidas;
+
+        bebidas = this.getResources().getStringArray(R.array.bebidas);
+        precio_bebidas = this.getResources().getStringArray(R.array.precio_bebidas);
+
+        for (String precio_bebida : precio_bebidas) {
+            contentValues.put("nombre_bebida", precio_bebida);
+        }
+
+        for (String bebida : bebidas) {
+            contentValues.put("precio", Float.parseFloat(bebida));
+        }
+
+        db.insert("Bebidas", null, contentValues);
     }
 
 }
