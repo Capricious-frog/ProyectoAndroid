6b75ab7702eac5983223946654e5012313faa0d0
Julen Ortiz <Julortizw@gmail.com>
2018-02-14 00:42:30 +0100

    ...

--- app/src/main/java/com/example/adminportatil/proyectoandroid/KebabsSQLiteHelper.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/KebabsSQLiteHelper.java
@@ -15,20 +15,16 @@ public class KebabsSQLiteHelper extends SQLiteOpenHelper {
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS 'Bebidas'");
         db.execSQL("DROP TABLE IF EXISTS \"Datos_cliente\"");
-        db.execSQL("DROP TABLE IF EXISTS 'Kebab'");
-        db.execSQL("DROP TABLE IF EXISTS 'Pedido_bebida'");
         db.execSQL("DROP TABLE IF EXISTS 'Pedido_kebab'");
+        db.execSQL("DROP TABLE IF EXISTS 'Pedido_bebida'");
 
         crearBd(db);
     }
 
     private void crearBd(SQLiteDatabase db){
-        db.execSQL("CREATE TABLE 'Bebidas' ( 'cod_bebida' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 'nombre_bebida' TEXT NOT NULL UNIQUE, 'precio' INTEGER NOT NULL )");
         db.execSQL("CREATE TABLE \"Datos_cliente\" ( 'cod_cliente' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 'Nombre' TEXT NOT NULL, 'Direccion' TEXT NOT NULL, 'Telefono' TEXT NOT NULL, 'Email' TEXT NOT NULL )");
-        db.execSQL("CREATE TABLE 'Kebab' ( 'cod_kebab' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 'tipo_kebab' TEXT NOT NULL UNIQUE, 'carne_kebab' TEXT NOT NULL UNIQUE, 'tamaño_kebab' TEXT NOT NULL UNIQUE, 'precio_tipo' INTEGER NOT NULL, 'precio_carne' INTEGER NOT NULL, 'precio_tamaño' INTEGER NOT NULL )");
-        db.execSQL("CREATE TABLE 'Pedido_bebida' ( 'cod_pedido' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 'nombre_bebida' TEXT NOT NULL UNIQUE, 'cantidad' INTEGER NOT NULL )");
         db.execSQL("CREATE TABLE 'Pedido_kebab' ( 'cod_pedido' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 'tipo_kebab' TEXT NOT NULL, 'tamaño_kebab' TEXT NOT NULL, 'tipo_carne' TEXT NOT NULL, 'cantidad' INTEGER NOT NULL)");
+        db.execSQL("CREATE TABLE 'Pedido_bebida' ( 'cod_pedido' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 'nombre_bebida' TEXT NOT NULL UNIQUE, 'cantidad' INTEGER NOT NULL )");
     }
 }
