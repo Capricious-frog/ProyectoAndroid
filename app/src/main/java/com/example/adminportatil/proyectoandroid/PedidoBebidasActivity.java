0a2bfda4665aeb0751d0090ebecf3bdfd0f8ebcd
Julen Ortiz <Julortizw@gmail.com>
2018-02-17 22:57:06 +0100

    ...

--- app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoBebidasActivity.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoBebidasActivity.java
@@ -1,15 +1,12 @@
 package com.example.adminportatil.proyectoandroid;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class PedidoBebidasActivity extends AppCompatActivity {
