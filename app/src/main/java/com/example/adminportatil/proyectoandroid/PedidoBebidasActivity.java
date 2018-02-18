684e0ae3eec6154f27c6ea9b045c9f64c83ea1a1
Julen Ortiz <Julortizw@gmail.com>
2018-02-18 13:10:40 +0100

    ...

--- app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoBebidasActivity.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/PedidoBebidasActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
-import java.util.ArrayList;
 
 public class PedidoBebidasActivity extends AppCompatActivity {
     int cod_pedido, cod_cliente;
