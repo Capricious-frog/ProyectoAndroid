0a53d84cb55aa483a686f41ab9e2bcbaaa35a107
Julen Ortiz <Julortizw@gmail.com>
2018-02-18 13:09:55 +0100

    ...

--- app/src/main/java/com/example/adminportatil/proyectoandroid/ResumenPedido.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/ResumenPedido.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class ResumenPedido extends AppCompatActivity {
@@ -42,11 +43,28 @@ public class ResumenPedido extends AppCompatActivity {
         String[] ccli = {String.valueOf(cod_cliente)};
         String[] cped = {String.valueOf(cod_pedido)};
 
+        ArrayList<String> tipo_kebab = new ArrayList<>();
+        ArrayList<String> tipo_carne = new ArrayList<>();
+        ArrayList<String> tamano = new ArrayList<>();
+
+        ArrayList<String> precio_tipo_kebab = new ArrayList<>();
+        ArrayList<String> precio_tipo_carne = new ArrayList<>();
+        ArrayList<String> precio_tamano = new ArrayList<>();
+
+        ArrayList<String> info_bebida = new ArrayList<>();
+        ArrayList<String> precio_info_bebida = new ArrayList<>();
 
 
-        Cursor datos_cliente = db.query("cliente", campos_datos, "cod_cliente = ?", ccli, null, null, null);
-        Cursor busqueda_kebabs = db.query("kebabs", campos_kebab, "cod_pedido = ?", cped, null, null, null);
-        Cursor busqueda_bebidas = db.query("bebidas", campos_bebida, "cod_pedido = ?", cped, null, null, null);
+        //Se recoge la informacion del pedido hecho por el cliente
+        Cursor datos_cliente = db.query("cliente", campos_datos, "pedido_completado = 0 AND cod_cliente = ?", ccli, null, null, null);
+        Cursor busqueda_kebabs = db.query("kebabs", campos_kebab, "pedido_completado = 0 AND cod_pedido = ?", cped, null, null, null);
+        Cursor busqueda_bebidas = db.query("bebidas", campos_bebida, "pedido_completado = 0 AND cod_pedido = ?", cped, null, null, null);
+
+        //Se recoge los diferentes tipos de kebab, carne, tamaño y nombre de bebidas. Cada uno se mete en su ArrayList correspondiente.
+        Cursor busqueda_tipo_kebab = db.query("tipo_kebab", campos_datos, "pedido_completado = 0 AND cod_cliente = ?", ccli, null, null, null);
+        Cursor busqueda_tipo_carne = db.query("tipo_carne", campos_kebab, "pedido_completado = 0 AND cod_pedido = ?", cped, null, null, null);
+        Cursor busqueda_tamano = db.query("tamano", campos_bebida, "pedido_completado = 0 AND cod_pedido = ?", cped, null, null, null);
+        Cursor busqueda_info_bebida = db.query("info_bebida", campos_bebida, "pedido_completado = 0 AND cod_pedido = ?", cped, null, null, null);
 
         //Guarda los datos del cliente
         if (datos_cliente.moveToFirst()) {
@@ -55,6 +73,42 @@ public class ResumenPedido extends AppCompatActivity {
                 nombre= datos_cliente.getString(0);
                 direccion= datos_cliente.getString(1);
             } while(datos_cliente.moveToNext());
+        }
+
+        //Guarda todos los tipos de kebab
+        if (busqueda_tipo_kebab.moveToFirst()) {
+            //Recorremos el cursor hasta que no haya más registros
+            do {
+                tipo_kebab.add(busqueda_tipo_kebab.getString(0));
+                precio_tipo_kebab.add(busqueda_tipo_kebab.getString(1));
+            } while(busqueda_tipo_kebab.moveToNext());
+        }
+
+        //Guarda todos los tipos de carne
+        if (busqueda_tipo_carne.moveToFirst()) {
+            //Recorremos el cursor hasta que no haya más registros
+            do {
+                tipo_carne.add(busqueda_tipo_carne.getString(0));
+                precio_tipo_carne.add(busqueda_tipo_carne.getString(1));
+            } while(busqueda_tipo_carne.moveToNext());
+        }
+
+        //Guarda todos los tipos de tamaños
+        if (busqueda_tamano.moveToFirst()) {
+            //Recorremos el cursor hasta que no haya más registros
+            do {
+                tamano.add(busqueda_tamano.getString(0));
+                precio_tamano.add(busqueda_tamano.getString(1));
+            } while(busqueda_tamano.moveToNext());
+        }
+
+        //Guarda todos los nombres de bebidas
+        if (busqueda_info_bebida.moveToFirst()) {
+            //Recorremos el cursor hasta que no haya más registros
+            do {
+                info_bebida.add(busqueda_info_bebida.getString(0));
+                precio_info_bebida.add(busqueda_info_bebida.getString(1));
+            } while(busqueda_info_bebida.moveToNext());
         }
 
         //Utilizando la información de la base de datos, rellenamos el edit text
@@ -141,6 +195,13 @@ public class ResumenPedido extends AppCompatActivity {
         texto.append("\nIVA: " + decimalFormat.format(precio * 0.21f) + "€");
         texto.append("\n\nTotal: " +  decimalFormat.format(precio + precio * 0.21f) + "€");
 
+        datos_cliente.close();
+        busqueda_kebabs.close();
+        busqueda_bebidas.close();
+        busqueda_tipo_kebab.close();
+        busqueda_tipo_carne.close();
+        busqueda_tamano.close();
+        busqueda_info_bebida.close();
     }
 
     public void enviar(View v){
