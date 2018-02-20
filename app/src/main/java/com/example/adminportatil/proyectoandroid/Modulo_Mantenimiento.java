a9d96887cada59402b9a867cece4f0d1f93b2c4e
Julen Ortiz <Julortizw@gmail.com>
2018-02-20 19:39:44 +0100

    ...

---
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/Modulo_Mantenimiento.java
@@ -0,0 +1,13 @@
+package com.example.adminportatil.proyectoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Modulo_Mantenimiento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modulo__mantenimiento);
+    }
+}
