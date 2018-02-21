17a7a893d42de708b1678ace385ebdd11b50fd16
Julen Ortiz <Julortizw@gmail.com>
2018-02-21 01:21:20 +0100

    ...

--- app/src/main/java/com/example/adminportatil/proyectoandroid/Actualizar.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/Actualizar.java
@@ -2,6 +2,7 @@ package com.example.adminportatil.proyectoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Actualizar extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class Actualizar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actualizar);
+    }
+
+    public void actualizarPedido(View view) {
+
+    }
+
+    public void actualizarTipoKebab(View view) {
+
     }
 }
--- app/src/main/java/com/example/adminportatil/proyectoandroid/Borrar.java
+++ app/src/main/java/com/example/adminportatil/proyectoandroid/Borrar.java
@@ -1,7 +1,11 @@
 package com.example.adminportatil.proyectoandroid;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class Borrar extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class Borrar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrar);
+    }
+
+    public void borrar(View view) {
+        KebabsSQLiteHelper kqlh = new KebabsSQLiteHelper(this);
+        SQLiteDatabase db = kqlh.getWritableDatabase();
+
+        RadioButton r1 = findViewById(R.id.radioButton);
+        RadioButton r2 = findViewById(R.id.radioButton2);
+        EditText codigo = findViewById(R.id.editText7);
+
+        if (r1.isChecked()) {
+            db.execSQL("DELETE FROM tipo_kebab WHERE cod_tipo_kebab = " + Integer.parseInt(codigo.getText().toString()) + "");
+        } else {
+            db.execSQL("DELETE FROM pedido WHERE cod_pedido = " + Integer.parseInt(codigo.getText().toString()) + "");
+        }
     }
 }
